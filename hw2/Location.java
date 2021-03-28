a213e34a1bfb06c8fe0cc6f62d90a0cb91e08c0b
KYBee <kyb3634@naver.com>
2021-03-28 19:22:06 +0900

    implement Location.equals, Location.hashCode

--- hw2/Location.java
+++ hw2/Location.java
@@ -23,4 +23,37 @@ public class Location
     {
         this(0, 0);
     }
+
+
+    /** Compare two points for equality. */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Location) {
+            Location target = (Location)o;
+            return getX() == target.getX() && getY() == target.getY();
+        }
+        return false;
+    }
+
+    /** generate same hash code if two locations return true in equals method*/
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + xCoord;
+        result = prime * result + yCoord;
+
+        return result;
+    }
+
+    /** Return the X coordinate of the point. */
+    public double getX() {
+        return xCoord;
+    }
+
+    /** Return the Y coordinate of the point. */
+    public double getY() {
+        return yCoord;
+    }
+
 }
