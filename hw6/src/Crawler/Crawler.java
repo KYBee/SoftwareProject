d16d38bc50868e903da29bab13c3cc4bfaf3f3b5
KYBee <kyb3634@naver.com>
2021-05-21 20:23:57 +0900

    set Crawler package

---
+++ hw6/src/Crawler/Crawler.java
@@ -0,0 +1,17 @@
+package Crawler;
+
+public class Crawler {
+
+    public static void main(String args[]) {
+
+        // TODO 예외 처리 해야함
+        for (int i = 0; i < args.length; i++) {
+            String urlParameter = args[0];
+            String depthParameter = args[1];
+        }
+
+
+
+    }
+
+}
